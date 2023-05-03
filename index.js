0351c1236409179b5eb7a73eaf425a714a7a7aa2
Preston Gregg <63014940+prestongit@users.noreply.github.com>
2023-05-03 14:35:14 +0900

    Create function that plays 5 games of rps and then shows the score of the game

--- index.js
+++ index.js
@@ -11,24 +11,40 @@ function getComputerChoice() {
     }
 }
 
+let winCounter = 0;
+let loseCounter = 0;
 
 function playRound(playerSelection, computerSelection) {
-    
     if (playerSelection == 'rock' && computerSelection == 'scissors' || playerSelection == 'scissors' && computerSelection == 'paper' || playerSelection == 'paper' && computerSelection == 'rock' ) {
+        winCounter+= 1;
         return `You Win! ${playerSelection} beats ${computerSelection}`
     }
     else if(playerSelection == computerSelection) {
         return "It's a tie!"
     } 
     else {
+        loseCounter+= 1;
         return `You Lose! ${computerSelection} beats ${playerSelection}`
     }
 
 }
 
-const playerSelection = 'paper'
+
+let playerSelection
 const computerSelection = getComputerChoice();
 
-console.log(playRound(playerSelection, computerSelection))
+function playGame() {
 
+    for (i=0; i<5; i++) {
+        playerSelection = window.prompt("Rock, paper, or scissors?")
 
+        const computerSelection = getComputerChoice();
+
+        console.log(playRound(playerSelection, computerSelection))
+        
+    }
+    return `The score is ${winCounter} to ${loseCounter}`
+
+    winCounter = 0;
+    loseCounter = 0;
+}
